44fb1ef895808951160fa18ff76d1f79cb6084b6
Andrii M <andreyam92@gmail.com>
2019-03-07 11:45:36 +0200

    Test tasks
    - Code refactor
    - Data format changed

--- src/spark_pipelines/io_services.py
+++ src/spark_pipelines/io_services.py
@@ -1,15 +1,18 @@
 import os
 import shutil
 
+OUTPUT_RESULT_FOLDER = "../resources/output/result"
+
 def read_df_from_file(spark_session ,file_name):
     return spark_session.read.option("header", "true") \
     .option("delimiter", " ") \
     .option("inferSchema", "true") \
     .csv(file_name)
 
-def read_df_to_csv_file(df, path_to_file):
+def write_df_to_csv_file(df, path_to_file):
+    clean_target_directory()
     df.write.csv(path_to_file, header=True, sep=" ")
 
 def clean_target_directory():
-    if (os.path.isdir("../../resources/output/result")):
-        shutil.rmtree("../../resources/output/result")
+    if (os.path.isdir(OUTPUT_RESULT_FOLDER)):
+        shutil.rmtree(OUTPUT_RESULT_FOLDER)
--- src/spark_pipelines/spark_services.py
+++ src/spark_pipelines/spark_services.py
@@ -8,6 +8,14 @@ APP_NAME = "adTestProject"
 def join_two_df(df_one, df_two, fields):
     return df_one.join(df_two, fields, how="left")
 
+def complicated_computation_two_df(df_one, df_two):
+    pass
+
+
+def complicated_computation_one_df(df_one):
+    pass
+
+
 def get_spark_session():
     sc = SparkContext(SPARK_MASTER, APP_NAME)
     return SparkSession(sc)
